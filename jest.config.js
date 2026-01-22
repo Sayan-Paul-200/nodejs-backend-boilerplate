c6d88f9a1f5854c51d9d51d98dd1d8f995b5f89e
Sayan Paul <sayanpaul666.ap@gmail.com>
2026-01-22 21:14:54 +0530

    fix: downgrade uuid to v9 and upgrade node to v24 for stability

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-
-  moduleNameMapper: {
-    "^uuid$": require.resolve("uuid"), 
-  },
   
   // 1. FIX TYPO: Change 'matchers' to 'testMatch'
   testMatch: ["**/*.test.ts"],
